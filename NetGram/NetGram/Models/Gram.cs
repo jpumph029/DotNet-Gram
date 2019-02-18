a05ff53765260a7eaab1a8b46dc96b80f026ce21
Joseph <pumphrey.joseph@gmail.com>
2019-02-18 15:32:40 -0800

    fixed a bug where the web app would throw an exeption when trying to delete or save with no input on the manage page

--- NetGram/NetGram/Models/Gram.cs
+++ NetGram/NetGram/Models/Gram.cs
@@ -10,7 +10,7 @@ namespace NetGram.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
-
+        
         public string URL { get; set; }
     }
 }
